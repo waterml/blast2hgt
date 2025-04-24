7452117325f1e3e35400f87f519b5e16308f6718
waterml <mengli.kib@gmail.com>
2025-04-24 15:25:15 +0800

    Migrate to mariadb

---
+++ extract_acc.taxid_from_nr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+db=nr
+blastdbcmd -db ~/ncbi/nr/nr -entry all -outfmt "%a %T" >nr.acc.taxid
+##blastdbcmd -db ~/ncbi/nr/nr -entry all -outfmt "%a %T %g" >nr.acc.taxid.gi
+perl -pe 's/\..*? / /' $db.acc.taxid |grep -v -e '=' -e ',' |sort --parallel=20 |uniq >$db.acc.taxid.lite
